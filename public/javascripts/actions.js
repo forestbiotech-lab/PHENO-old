e299ad27f696f2be3f43f886eee37cd7dba6bb98
Bruno Costa <netbofia@hotmail.com>
2016-12-06 17:31:00 +0000

    Layout for front page. With hover event

---
+++ public/javascripts/actions.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  //Set hover event for menu.
+  $('.menu span.menuItem1').mouseover(function(){$(this).toggleClass('hover')}).mouseout(function(){$(this).toggleClass('hover')});
+
+});
