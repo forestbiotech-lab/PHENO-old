040cd9fc034b371fadaabe0a41494d868c51a72c
Bruno Costa <netbofia@hotmail.com>
2017-07-04 16:47:06 +0100

    Dealing with error catching gets the error code. 501 if not detailed in last catch.

--- routes/api.js
+++ routes/api.js
@@ -9,20 +9,16 @@ var germplasmCalls = require('./../components/brapi/germplasmCalls');
 // germplasm-search
 router.get('/germplasm-search', function(req, res, next){
   var query=req.query;
-    germplasmCalls(query).then(function(germplasmRes){
+  germplasmCalls(query).then(function(germplasmRes){
 
-      //The send isn't sending the error but status is ok.
-      germplasmRes instanceof Error ? 
-      res.status(400).send("Error") : 
-      res.status(200).json(germplasmRes);
+    res.status(200).json(germplasmRes);
 
-       //This isn't placed in the right place. Does the done catch the error? 
-/*    .catch(function(err){
-      console.log("germplasm-search - Err model not implemented: ");
-      res.status(err.status || 500);
-      res.render(err);
-    })*/
+  }).catch(function(err){
+
+    res.status(parseInt(err.metadata.status[0].code || 501 )).json(err);
+
   })
+  
 });
 
 //The studies Call
