09c367d5d95d5b6aa601b61d33834d95bffabe90
Bruno Costa <netbofia@hotmail.com>
2017-07-12 17:05:26 +0100

    Simplified with new fmtFunctions

--- components/brapi/cropCall.js
+++ components/brapi/cropCall.js
@@ -1,9 +1,11 @@
 /**
  * Created by João M. F. Cardoso on 11/07/2017.
+ * Modified by Bruno Costa on 12/07/2017
  */
 
 /* Imports */
 var models = require('./models');
+var fmtFunc= require('./formatingFunctions')
 
 /* Methods connecting to the database */
 module.exports = function (query) {
@@ -12,72 +14,39 @@ module.exports = function (query) {
     query.pageSize = query.pageSize || 1000;    //The default in specs is 1000
     query.page = query.page || 0; // Default is 0
     query.offset= query.page * query.pageSize;
-    console.log(query);
 
     /* Calls a function from the model to fetch data from the database */
     return new Promise(function(resolve,reject){
-        models.getCrops(query).then(function (response) {
+        models.getCrops(query).then(function (res) {
 
             /* Error response */
-            if (!(response instanceof Error)) {
+            if (!(res instanceof Error)) {
+
                 /* Create the response according to the API */
                 var databaseValues = [];
                 /* The variable where the database values will be stored */
 
                 /* Filling the database values */
-                for (valueKey in response.rows) {
-                    databaseValues.push(response.rows[valueKey].dataValues.commonCropName)
+                for (valueKey in res.rows) {
+                    databaseValues.push(res.rows[valueKey].dataValues.commonCropName)
                 }
+                        //Generate pagination details
+                var pagination=fmtFunc.generatePagination(res,query);
 
-                //Send fulfilled promise
-                resolve({
-                    "metadata": {
-                        "status": [{code: 500, message: response}],
-                        "datafiles": [],
-                        "pagination": {
-                            "pageSize": response.rows.length,
-                            "currentPage": query.page,  //This might produce errors if query var changes after promise resolves. Not sure if this is an issue.
-                            "totalCount": response.count,
-                            "totalPages": Math.ceil(response.count / query.pageSize) /* Calculates the number of pages */
-                        }
-                    },
-                    "result": {
-                        "data": databaseValues
-                    }
-                })
+                //Args:queryData,pagination,code,message
+                resolve(fmtFunc.generateJSON(databaseValues,pagination,200,null));
+                
             } else {
                 //Send the error in the status send rejection to promise
-                reject({
-                    "metadata": {
-                        "status": [{code: 500, message: response}],
-                        "datafiles": [],
-                        "pagination": {
-                            "pageSize": response.rows.length,
-                            "currentPage": query.page,  //This might produce errors if query var changes after promise resolves. Not sure if this is an issue.
-                            "totalCount": response.count,
-                            "totalPages": 1 //This must be calculated another call with the same attributes and no limit to count.
-                        }
-                    },
-                    "result": {
-                        "data": null
-                    }
-                });
+                //Args:queryData,pagination,code,message
+                reject( fmtFunc.generateJSON(null,null,400,res) );
+                
             }
         }).catch(function(err){
-            reject({
-                "metadata": {
-                    "status": [{code:500,"message":err}],  //Some other status?
-                    "pagination": {
-                        "pageSize": err,//.length,
-                        "currentPage": query.page,  //This might produce errors if query var changes after promise resolves. Not sure if this is an issue.
-                        "totalCount": err,//.length,
-                        "totalPages": 1 //This must be calculated another call with the same attributes and no limit to count.
-                    }
-                },
-                "result":{
-                    "data": null
-                }
-            })
+           
+          //queryData,pagination,code,message
+          reject( fmtFunc.generateJSON(null,null,500,err) );        
+
         });
 
     })
