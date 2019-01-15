fc9efc81beb68d898c68a2ec6a9295aee2aaa8c3
Bruno Costa <netbofia@hotmail.com>
2019-01-15 17:03:06 +0000

    Updates the where helper to use operations instead of alias.

--- components/brapi/helpers/formatWhereAttribute.js
+++ components/brapi/helpers/formatWhereAttribute.js
@@ -1,12 +1,26 @@
-module.exports= function formatWhereAttribute(attribute,type){
+Sequelize=require('sequelize');
+const Op = Sequelize.Op;
+
+module.exports= function formatWhereAttribute(attribute,operator){
+  //http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operator=Op[operator]
   result={}
   if(typeof attribute == "string"){
     attribute=[attribute]
   }else if(typeof attribute == "object"){
-    attribute=attribute || ""
+    if( attribute instanceof Array ){
+      let sanityCheck=[]
+      for(a in attribute){
+        let item=attribute[a]
+        if(typeof item == "string") sanityCheck.push(item) 
+      }        
+      attribute=sanityCheck;
+    }else{ //If it's not an array then it not within specs and should be empty to ensure sanity
+      attribute=[""]
+    }
   }
   if(attribute){
-    result[type]=attribute;
+    result[operator]=attribute;
     return result
   }
 }
