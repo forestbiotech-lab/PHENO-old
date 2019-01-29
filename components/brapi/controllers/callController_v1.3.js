cdc3004855dfe94948fce00c54bd0cf714c4572c
Bruno Costa <netbofia@hotmail.com>
2019-01-29 18:11:51 +0000

    Adds current state of the implementation of v1.3 calls

---
+++ components/brapi/controllers/callController_v1.3.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Bruno Costa on 20-06-2018.
+ */
+
+var dateFormat = require('dateformat');
+var models= require('./../v1.3/models_v1.3');
+var fmtFunc= require('./../formatingFunctions');
+var fmtRetreivedData= require('./../helpers/formatRetreivedData')
+
+module.exports = function(options,call,callback){
+
+  var options= options || {body:{},params:{},query:{},where:{}};
+  var query=options.query
+  var attributes={}
+
+  attributes.pageSize = query.pageSize || 1000     //The default in specs is 1000    
+  attributes.page = query.page || 0 
+  //Set the page to show
+  attributes.offset = attributes.page * attributes.pageSize;
+  attributes.where = options.where;
+
+
+
+  //Runs a model function with options if they exist
+
+  //Now this promise will send the call and the logic of parsing the result will be set here.
+  return new Promise(function(resolve,reject){
+
+    models[call](attributes).then(function(res){
+    
+      if(res instanceof Error){
+          //Send the error in the status send rejection to promise
+          //Args:queryData,pagination,code,message
+          reject( fmtFunc.generateJSON(null,null,400,res.name+" : "+res.message) );
+   
+      }else{
+          //Send res back to get the metadata, uniqueId Attribute and the callStructure
+          var callSpecificData=callback(res)
+          var data=fmtRetreivedData(callSpecificData,res)
+          var pagination=fmtFunc.generatePagination(res,attributes);
+          resolve(fmtFunc.generateJSON(data,pagination,200,null));
+
+      }//end else
+      //end then
+    }).catch(function(err){
+      //queryData,pagination,code,message
+      console.trace(err)
+      reject( fmtFunc.generateJSON(null,null,500,err.name+" : "+err.message) );
+
+    })
+
+  })
+  
+}
+
+
---
+++ components/brapi/structures/v1.3/Samples_SampleDbId.js
@@ -0,0 +1,15 @@
+module.exports={
+  germplasmDbID: {_table:["ObservationUnit","Study","StudyGermplasm"],_attribute:'germplasmId'},
+  //notes: '',
+  observationUnitDbId: {_table:'ObservationUnit',_attribute:'id'},
+  //plantDbId: {_table:['SamplePlant','Plant'],_attribute:'id'},
+  //plateDbId: {_table:},
+  //plateIndex: {}
+  //plotDbId: {_table:['SamplePlant','Plant','Plot'],_attribute:'id'},
+  sampleDbId: 'id',
+  //sampleTimestamp: 'sampleDate',
+  //sampleType: '',
+  //studyDbId: '',  
+  //takenBy: { _table:'Person', _attribute:"name" },
+  //tissueType: ''
+}
