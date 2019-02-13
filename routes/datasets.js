5f584c4ccc2e9f502bda1a56142938d897e9eac2
Bruno Costa <netbofia@hotmail.com>
2019-02-13 23:56:33 +0000

    Add debug for catch in introduced promise. Breaks the study card into multiple promisses to build the data to be sent to frontend.

--- routes/datasets.js
+++ routes/datasets.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var resolveHelper = require('./../components/brapi/helpers/resolveHelper');
+var getOptions = require('./../components/brapi/helpers/getOptions');
 var showDataset = require('./../components/brapi/datasets/global')
 var germplasm = require('./../components/brapi/datasets/germplasm')
 var study = require('./../components/brapi/datasets/study')
+var relatedGermplasms = require('./../components/brapi/datasets/relatedGermplasms')
+var relatedStudies = require('./../components/brapi/datasets/relatedStudies')
+var observationVariables = require('./../components/brapi/datasets/observationVariables')
+var studyAdditionalInfo = require('./../components/brapi/datasets/studyAdditionalInfo')
+var locationAdditionalInfo = require('./../components/brapi/datasets/locationAdditionalInfo')
 
 var resolveCall=resolveHelper.resolveCall
+
+var debug = require('debug')
+var debug_std = debug('brapi:server');
+var debug_full= debug('brapi:trace');
 
 const map=require("svg-world-map")
 const toHTML=require('vdom-to-html')
@@ -29,16 +39,45 @@ router.get('/germplasm/:germplasmId',function(req,res,next){
 })
 
 router.get('/study/:studyId',function(req,res,next){
-  var errMsg="Router dataset Get germplsm - "
+  var errMsg="Router dataset Get study - "
   var call=study;
   formatResponse=function(response){
-  	console.log(response.result.data[0])
- 	return {
-      	title: "PHENO - Study info",
-      	host: req.headers.host,
-      	studyData:response.result.data[0],
-    }
+  	var studyData=response.result.data[0]
+  	location=studyData.location
+    myMap=map(location.longitude,location.latitude)
+    svg=toHTML(myMap)
+  	let fakeReq=getOptions({
+  		params:{
+  			programId:studyData.program.id,
+  			studyId:studyData.id,
+  			locationId:studyData.program.lead_person.institution.locationId
+  		}
+  	})
+  	addonCalls=[
+  		relatedStudies(fakeReq),
+  		relatedGermplasms(fakeReq),
+  		observationVariables(fakeReq),
+  		studyAdditionalInfo(fakeReq),
+  		locationAdditionalInfo(fakeReq)
+  	];
+  	
+  	return Promise.all(addonCalls).then(function(callRes){
+  		data={
+	      	title: "PHENO - Study info",
+	      	host: req.headers.host,
+	      	studyData:studyData,
+	      	map:svg,
+	      	relatedStudies:null
+	    }
+  		for (i in callRes){
+  			Object.assign(data,callRes[i].result.data[0])
+  		}
+		return data	
+  	}).catch(function(err){
+  		debug_std("Router | dataset | study | One of the promises failed - err: "+err)
+  	})	
   }	
+
   resolveCall(call,req,res,errMsg,"study",formatResponse)
 })
 
@@ -50,7 +89,11 @@ router.get('/global/:datasetId',function(req,res,next){
   resolveCall(call,req,res,errMsg,'showDataset');
 })
 
-
+router.get("/addon/relatedStudies/:programId",function(req,res,next){
+	var errMsg="Router | datasets | addon relatedStudies - err: "
+	var call=relatedStudies
+	resolveCall(call,req.res.errMsg) 
+})
 
 
 
