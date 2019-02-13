3214e7a341605ec119fab65e6532325e1564f468
Bruno Costa <netbofia@hotmail.com>
2019-02-13 17:11:18 +0000

    Recreated the req options from scratch to pass to the controller. Previously it only cleaned up the req

--- components/brapi/helpers/getOptions.js
+++ components/brapi/helpers/getOptions.js
@@ -1,8 +1,8 @@
 module.exports = function(req){
 	//Exports cleaned request
 	result={}
-	result.body=req.body;
-	result.params=req.params;
-	result.query=req.query;
+	result.body=req.body || {}
+	result.params=req.params || {};
+	result.query=req.query || {};
 	return result;
 }
