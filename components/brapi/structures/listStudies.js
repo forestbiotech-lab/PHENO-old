f0aa2370d38313b085d8372404e46e6981ef0c3b
Bruno Costa <netbofia@hotmail.com>
2019-02-22 00:53:02 +0000

    Fix #23 | This isn't the main commit with the changes. | Add hyperlink to trials

--- components/brapi/structures/listStudies.js
+++ components/brapi/structures/listStudies.js
@@ -1,5 +1,6 @@
 module.exports={
   programId:{_table:["Trial","Program"],_attribute:"id"}, 
+  trialId:{_table:"Trial",_attribute:"id"},
   trialName:{_table:"Trial",_attribute:"name"},
   studyName:"name", 
   studyId:"id",
