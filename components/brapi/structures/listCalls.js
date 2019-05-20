b5b6b8e0292ae71a18e6c1c9dd200c9408660d35
Bruno Costa <netbofia@hotmail.com>
2019-05-20 19:48:25 +0100

    Attempts to fix /calls which wasn't exposing the dataTypes in the call

--- components/brapi/structures/listCalls.js
+++ components/brapi/structures/listCalls.js
@@ -1,5 +1,5 @@
 module.exports={
 	call:"path",
-	dataTypes: [{_table:"DataTypes_table",_attribute:"dataType"}],
+	dataTypes: [{_table:"DataType",_attribute:"dataType"}],
 	methods: [{_table:"Method",_attribute:"method"}],
 }
