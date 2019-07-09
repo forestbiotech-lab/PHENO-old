2b134d5de87b0d5bde2958d8ded78f57427c7c56
Bruno Costa <brunocosta@itqb.unl.pt>
2019-07-09 19:35:31 +0100

    Minor fix. Also closes  Fix #26

--- components/brapi/v1.3/trials_GET.js
+++ components/brapi/v1.3/trials_GET.js
@@ -9,49 +9,47 @@ module.exports = function(options){
   var options= options || {body:{},params:{},query:{}};  
   options.where={}
 
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   call="trials"
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   //Where logic
   //Do this for each where attribute needed.
 
-//-- active (Optional, ) ... Filter active status true/false.
-//sortBy (Optional, ) ... Sort order. Name of the field to sorty by.
-//sortOrder (Optional, ) ... Sort order direction: asc/desc
+  //sortBy (Optional, ) ... Sort order. Name of the field to sorty by.
+  //sortOrder (Optional, ) ... Sort order direction: asc/desc
 
-//|||||||||||||||TrialId|||||||||||||||||||||||
+  //|||||||||||||||TrialId|||||||||||||||||||||||
   options.params.trialDbId ? metadata={ metadataOnlyRemoveData:true } : metadata={}
   attribute=options.params.trialDbId || options.query.trialDbId;
   var value=fmtWhereAttr(attribute,"eq")
   if ( value != null )
     options.where.id=value 
   delete options.query.id;
-//||||||||||||||commonCropName|||||||||||||||||
+  //||||||||||||||commonCropName|||||||||||||||||
   attribute=options.query.commonCropName
   var value=fmtWhereAttr(attribute,"eq")
   if ( value != null )
     options.where["$Study.StudyGermplasm.Germplasm.Species.Crop.commonCropName$"]=value 
   delete options.query.commonCropName;
-//||||||||||||||programDbId|||||||||||||||||||
+  //||||||||||||||programDbId|||||||||||||||||||
   attribute=options.query.programDbId;
   var value=fmtWhereAttr(attribute,"eq")
   if ( value != null )
     options.where.programId=value 
   delete options.query.programDbId;
-//|||||||||||||||||locationDbId||||||||||||||
+  //|||||||||||||||||locationDbId||||||||||||||
   attribute=options.query.locationDbId
   var value=fmtWhereAttr(attribute,"eq")
   if ( value != null )
     options.where["$Study.locationId$"]=value 
-  delete options.query.locationDbId;
-//|||||||||||||||||active||||||||||||||
+  delete options.query.locationDbId; 
+  //|||||||||||||||||active||||||||||||||
   attribute=options.query.active
   var value=fmtWhereAttr(attribute,"eq")
   if ( value != null )
     options.where.active=value 
   delete options.query.active;
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   return controller(options,call,callback)
   
 }
