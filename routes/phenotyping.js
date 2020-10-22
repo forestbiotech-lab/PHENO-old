b95a1958b8cd2d1ed339941dc9229fca5250dac6
Bruno Costa <brunovasquescosta@gmail.com>
2020-10-22 18:35:45 +0100

    Experimental intro of data

--- routes/phenotyping.js
+++ routes/phenotyping.js
@@ -10,7 +10,7 @@ router.get('/setup',function(req,res,next){
 })
 
 router.get('/plant/set/:study/:plot/:block/:row/:pot',function(req,res,next){
-   res.render('plant',req.params)
+   res.render('plant2',req.params)
 })
 
 module.exports = router;
